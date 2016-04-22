a64cde9d55a1f654fde32551beeba122715be3f8
Daehyun You <daehyun9176@icloud.com>
2016-04-22 11:07:42 +0200

    Add new project

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Daehyun You on 11/29/15.
+//
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+  std::cout << "hello would" << std::endl;
+  std::cout << argv[1] << std::endl;
+  return 0;
+}
