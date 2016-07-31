ef49fc94b86df800f76053ff7e96809a607d1674
Daehyun You <daehyun9176@gmail.com>
2016-08-01 04:47:22 +0900

    Fix a bug

--- Macros/fg.C
+++ Macros/fg.C
@@ -1,5 +1,5 @@
 #include "FitGauss.h"
 
 void fg(double reg1=0, double reg2=0) {
-   auto *fitGauss = new FitGauss(reg1, reg2);
+   FitGauss *fitGauss = new FitGauss(reg1, reg2);
 }
--- Macros/projx.C
+++ Macros/projx.C
@@ -1,5 +1,5 @@
 #include "Proj.h"
 
 void projx(double reg1=0, double reg2=0) {
-  auto *proj = new Proj(Proj::AxisType::x, reg1, reg2);
+  Proj *proj = new Proj(Proj::AxisType::x, reg1, reg2);
 }
