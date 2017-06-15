31236dfafd82f84b8294a202861f2064f2923e49
David July <david.july1991@gmail.com>
2017-06-15 10:23:58 +0900

    add powershell scripts to run the docker images
    rename shell scripts

--- sort.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -ti --rm -v HOST:DOCKER david9107/sp8-analysis sp8sort $*
---
+++ sp8ana.ps1
@@ -0,0 +1,6 @@
+function nixpath {
+    $resolved = Resolve-Path $args
+    Write-Output "/$( $resolved.Path.Replace(':\', '/').Replace('\', '/') )".Replace('//', '/')
+}
+
+docker run -ti --rm -v HOST:DOCKER david9107/sp8-analysis sp8ana $(nixpath $args)
